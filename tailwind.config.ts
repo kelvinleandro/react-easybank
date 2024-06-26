0228ffef0046bd72da1c21e8781187c495266384
kelvinleandro <kelvinleandro5@gmail.com>
2024-06-26 16:44:32 -0300

    feat: add colors and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'darkBlue': "#2D314D",
+        'limeGreen': "#31D35C",
+        'brightCyan': "#2BB7DA",
+        'grayishBlue': "#9698A6",
+        'lightGrayishBlue': "#F3F4F6",
+        'veryLightGray': "#FAFAFA",
+      }
     },
   },
   plugins: [],
